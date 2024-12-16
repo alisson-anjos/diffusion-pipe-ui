172ce6fe9f83a8cefdf7ea20ddd0593f8898f8bc
Alisson Pereira Anjos <117223069+alisson-anjos@users.noreply.github.com>
2024-12-16 17:38:15 +0000

    Update

--- diffusion_pipe_ui/__init__.py
+++ diffusion_pipe_ui/__init__.py
@@ -1,17 +1,18 @@
 from diffusion_pipe_ui.training import train_lora, stop_training, TrainingManager
-from diffusion_pipe_ui.interface import create_interface
+from diffusion_pipe_ui.interface import build_interface
 from diffusion_pipe_ui.config import CONFIG_HISTORY_DIR
 from diffusion_pipe_ui.utils import generate_unique_filename
 from diffusion_pipe_ui.dataset import create_dataset_config
-from diffusion_pipe_ui.download import download_file
+from diffusion_pipe_ui.download import download_output_zip, download_dataset_action
 
 __all__ = [
     'train_lora',
     'stop_training',
     'TrainingManager',
-    'create_interface',
+    'build_interface',
     'CONFIG_HISTORY_DIR',
     'generate_unique_filename',
     'create_dataset_config',
-    'download_file'
+    'download_output_zip',
+    'download_dataset_action'
 ]
