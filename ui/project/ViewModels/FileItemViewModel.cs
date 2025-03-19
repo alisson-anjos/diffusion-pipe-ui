d3862e2bd1118ed1bc3930b07528d58793f7ddd3
alisson-anjos <alissonerdx@gmail.com>
2025-03-19 23:19:01 +0000

    Add FileExplorer

---
+++ ui/project/ViewModels/FileItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DiffusionPipeInterface.ViewModels
+{
+    public class FileItemViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool IsDirectory { get; set; }
+        public bool IsExpanded { get; set; }
+        public bool IsSelected { get; set; }
+        public string Icon { get; set; } = string.Empty;
+        public string IconExpanded { get; set; } = string.Empty;
+        public List<FileItemViewModel> Children { get; set; } = new List<FileItemViewModel>();
+        public string FullPath { get; set; } = string.Empty;
+    }
+}
